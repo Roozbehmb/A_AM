772b06f242b75c8f547f7ed462273955df4cc4b6
Roozbeh <Roozbehmbspeed@gmail.com>
2023-11-20 00:57:36 +0330

    Read imported function names

--- include/parser.h
+++ include/parser.h
@@ -43,3 +43,36 @@ void parseExportTable(std::ifstream& file, const IMAGE_EXPORT_DIRECTORY& exportD
         }
     }
 }
+
+void parseImportTable(std::ifstream& file, const IMAGE_IMPORT_DESCRIPTOR& importDescriptor) {
+    std::cout << "\nImport Table:" << std::endl;
+
+    file.seekg(importDescriptor.Name, std::ios::beg);
+    std::string importedLibraryName;
+    std::getline(file, importedLibraryName, '\0');
+    std::cout << "Imported Library: " << importedLibraryName << std::endl;
+
+    file.seekg(importDescriptor.OriginalFirstThunk, std::ios::beg);
+    std::vector<uint32_t> thunkData;
+    while (true) {
+        uint32_t thunk;
+        file.read(reinterpret_cast<char*>(&thunk), sizeof(uint32_t));
+        if (thunk == 0) {
+            break;
+        }
+        thunkData.push_back(thunk);
+    }
+
+    for (uint32_t thunk : thunkData) {
+        if (thunk & 0x80000000) {
+            // Ordinal
+            std::cout << "Imported Function (Ordinal): " << (thunk & 0xFFFF) << std::endl;
+        } else {
+            // By Name
+            file.seekg(thunk + 2, std::ios::beg); // Skip hint
+            std::string importedFunctionName;
+            std::getline(file, importedFunctionName, '\0');
+            std::cout << "Imported Function (ByName): " << importedFunctionName << std::endl;
+        }
+    }
+}
