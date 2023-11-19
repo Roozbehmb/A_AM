52e9d07fe36b620c9dc7512c9aaaf5ac51a31646
Roozbeh <Roozbehmbspeed@gmail.com>
2023-11-20 00:31:33 +0330

    complate class BinarySearchTree

--- include/bst-File.h
+++ include/bst-File.h
@@ -8,3 +8,42 @@ struct TreeNode {
     
     TreeNode(const string& name) : data(name), left(nullptr), right(nullptr) {}
 };
+
+class BinarySearchTree {
+public:
+    BinarySearchTree() : root(nullptr) {}
+
+    void insert(const string& name) {
+        root = insertRecursive(root, name);
+    }
+
+    void display() {
+        inOrderTraversal(root);
+        cout << endl;
+    }
+
+private:
+    TreeNode* root;
+
+    TreeNode* insertRecursive(TreeNode* node, const string& name) {
+        if (node == nullptr) {
+            return new TreeNode(name);
+        }
+
+        if (name < node->data) {
+            node->left = insertRecursive(node->left, name);
+        } else if (name > node->data) {
+            node->right = insertRecursive(node->right, name);
+        }
+
+        return node;
+    }
+
+    void inOrderTraversal(TreeNode* node) {
+        if (node != nullptr) {
+            inOrderTraversal(node->left);
+            cout << "name:" << node->data <<endl;
+            inOrderTraversal(node->right);
+        }
+    }
+};
