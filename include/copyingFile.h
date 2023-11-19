0ede65a3e525963d3df3d9b5ad59802da32a9b7a
Roozbeh <Roozbehmbspeed@gmail.com>
2023-11-20 00:40:12 +0330

    Get the path to the executable

--- include/copyingFile.h
+++ include/copyingFile.h
@@ -4,5 +4,10 @@
 #include <Shlobj.h>
 
 int dataFile(){
-    
+    int number = 0;
+    std::cout << "plese enter a number :"<<std::endl;
+    std::cin >> number;
+    wchar_t exePath[MAX_PATH];
+    GetModuleFileNameW(NULL, exePath, MAX_PATH);
+
 }
