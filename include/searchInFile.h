101fa5e9d9285585957d1db991080aabcc65bee2
Roozbeh <Roozbehmbspeed@gmail.com>
2023-11-20 00:44:59 +0330

    add Function searchInFile

--- include/searchInFile.h
+++ include/searchInFile.h
@@ -7,3 +7,19 @@
 namespace fs = std::filesystem;
 
 using namespace std;
+
+void searchInFile(const std::string& filePath, const std::string& searchPhrase) {
+    
+    std::ifstream file(filePath);
+    if (file.is_open()) {
+        std::string line;
+        int lineCount = 0;
+        while (getline(file, line)) {
+            lineCount++;
+            if (line.find(searchPhrase) != std::string::npos) {
+                std::cout << "Match found in file: " << filePath << " (Line " << lineCount << ")\n";
+            }
+        }
+        file.close();
+    }
+}
