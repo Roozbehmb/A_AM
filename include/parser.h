4afaba7dfe9de748d16b867365b292f71410af13
Roozbeh <Roozbehmbspeed@gmail.com>
2023-11-20 00:55:30 +0330

    create Parser

---
+++ include/parser.h
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <cstdint>
+#include <cctype>  
+// #include "listNames.h"  
+
+using namespace std;
